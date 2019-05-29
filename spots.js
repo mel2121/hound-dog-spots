794a4d1c5e6c1eb23790ed304e5d7fad7b8bd6e6
mel2121 <37003211+mel2121@users.noreply.github.com>
2019-05-29 13:17:46 -0400

    tree practice

---
+++ spots.js
@@ -0,0 +1,41 @@
+//binary tree practice
+//node class
+class Node(){
+  constructor(data){
+    this.data=data;
+    this.left=null;
+    this.right=null;
+  }
+}
+//binary tree class
+class BinTree(){
+  constructor(){
+    this.root=null;
+  }
+insert(data){
+    var newNode=new Node(data);
+    if(this.root==null){
+      this.root=newNode;
+    } else {
+      this.insertNode(this.root,newNode);
+    }
+  }
+  insertNode(node,newNode){
+    if(node.data>newNode.data){
+      if(node.left===null){
+        node.left=newNode;
+      } else {
+        insertNode(node.left, newNode);
+      }
+      } else {
+        if(node.right==null){
+          node.right=newNode;
+        } else {
+          insertNode(node.right,newNode);
+        }
+      }
+    
+    
+  }
+  
+}
